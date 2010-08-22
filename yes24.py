0b7baab6531597bb591f8d84fee4bb73ae3fef3d
Kim, Jang-hwan <janghwan@gmail.com>
2010-08-22 22:28:32 +0900

    refactored class

--- yes24.py
+++ yes24.py
@@ -5,198 +5,129 @@ import re
 from BeautifulSoup import BeautifulSoup
 import logging
 
-class Yes24:
-    default_url = "http://www.yes24.com/"
-    secure_url  = "https://www.yes24.com/"
-    login_url   = "https://www.yes24.com/Templates/FTLogIn.aspx"
-    order_path  = "/Member/FTMyOrderList01.aspx"
-    order_url   = secure_url + order_path
-    order_detail_url = "https://www.yes24.com/Member/FTMyOrderDtl01.aspx"
-    login_data = {
-        "SMemberID"              : None,
-        "SMemberPassword"        : None,
-        "RefererUrl"             : "http://www.yes24.com/Main/Default.aspx",
-        "AutoLogin"              : "1",
-        "LoginIDSave"            : "N",
-        "FBLoginSub:LoginType"   : '',
-        "FBLoginSub:ReturnURL"   : '',
-        "FBLoginSub:ReturnParams": '',
-    }
+##
+## Constants
+##
+default_url = "http://www.yes24.com/"
+secure_url  = "https://www.yes24.com/"
+login_url   = "https://www.yes24.com/Templates/FTLogIn.aspx"
+order_path  = "/Member/FTMyOrderList01.aspx"
+order_url   = secure_url + order_path
+order_detail_url = "https://www.yes24.com/Member/FTMyOrderDtl01.aspx"
+login_data = {
+    "SMemberID"              : None,
+    "SMemberPassword"        : None,
+    "RefererUrl"             : "http://www.yes24.com/Main/Default.aspx",
+    "AutoLogin"              : "1",
+    "LoginIDSave"            : "N",
+    "FBLoginSub:LoginType"   : '',
+    "FBLoginSub:ReturnURL"   : '',
+    "FBLoginSub:ReturnParams": '',
+}
 
-    # 
-    open_url_count = 0
-    read_bytes = 0
+# used counting url_open
+open_url_count = 0
+read_bytes = 0
 
-    @staticmethod
-    def get_order_detail_link(order_id):
-        return Yes24.order_detail_url + "?ordNoH=" + order_id
+def get_order_detail_link(order_id):
+    return order_detail_url + "?ordNoH=" + order_id
 
-    @staticmethod
-    def get_deliver_state_link(order_id):
-        return Yes24.default_url + 'Order/FTDelvTrcListFrame.aspx?OID='+order_id+"&TTL=L"
+def get_deliver_state_link(order_id):
+    return default_url + 'Order/FTDelvTrcListFrame.aspx?OID='+order_id+"&TTL=L"
 
-    @classmethod
-    def open_url(cls, opener, url):
-        logging.debug('opening url: %s' % url)
-        error_count = 3
-        for retry_count in range(error_count):
-            try:
-                site = opener.open(url)
-                cls.open_url_count += 1
-                break
-            except urllib2.URLError, e:
-                logging.error("#%d failed to open url %s, retyring.." % retry_count+1, url)
-                continue
+def open_url(opener, url):
+    global open_url_count, read_bytes
+    logging.debug('opening url: %s' % url)
+    error_count = 3
+    for retry_count in range(error_count):
+        try:
+            site = opener.open(url)
+            open_url_count += 1
+            break
+        except urllib2.URLError, e:
+            logging.error("#%d failed to open url %s, retyring.." % retry_count+1, url)
+            continue
 
-        text = site.read()
-        cls.read_bytes += len(text)
-        logging.debug('read %d bytes' % len(text))
-        return text.decode('cp949')
+    text = site.read()
+    read_bytes += len(text)
+    logging.debug('read %d bytes' % len(text))
+    return text.decode('cp949')
 
+def authorize(username, password):
+    ''' authorize Yes24 with username and password, and return the opener '''
+    cookie_jar = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie_jar))
 
-    @staticmethod
-    def authorize(username, password):
-        ''' authorize Yes24 with username and password, and return the opener '''
-        cookie_jar = cookielib.CookieJar()
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie_jar))
-    
-        Yes24.login_data['SMemberID']       = username
-        Yes24.login_data['SMemberPassword'] = password
-    
-        logging.debug('opening url: %s' % Yes24.login_url)
-        resp = opener.open(Yes24.login_url, urllib.urlencode(Yes24.login_data))
-        html = resp.read()
-        logging.debug('read %d bytes' % len(html))
-    
-        Yes24.login_data['SMemberID']       = None
-        Yes24.login_data['SMemberPassword'] = None
-        del username, password
+    login_data['SMemberID']       = username
+    login_data['SMemberPassword'] = password
 
-    
-        if 'location.replace' not in html:
-            logging.warning('"location.replace" not found in response body. maybe login fail?')
-            return None
-        else:
-            return opener
+    logging.debug('opening url: %s' % login_url)
+    resp = opener.open(login_url, urllib.urlencode(login_data))
+    html = resp.read()
+    logging.debug('read %d bytes' % len(html))
 
-    @staticmethod
-    def verify_login(opener, url):
-        html = opener.open(url).read()
-        # 로그아웃 돼 있으니 login하라고 뜬다
-        if   'Login'  in html:  return False
-        # 로그인 돼 있으니 logout할 수 있다고 뜬다
-        elif 'Logout' in html:  return True
-        else:
-            raise Exception("로그인 여부를 판단할 수 없습니다.")
+    login_data['SMemberID']       = None
+    login_data['SMemberPassword'] = None
+    del username, password
 
 
-class OrderListPage:
-    @staticmethod
-    def retrieve_order_list_pages(opener, path):
-        ''' yields text of next order list page '''
-        while path is not None:
-            text = Yes24.open_url(opener, Yes24.secure_url + path)
-            yield text
-            # find out next page
-            page_no, path = OrderListPage.Parse.navi_info(text)
-    
-    @staticmethod
-    def retrieve_orders(opener, target_month):
-        ''' yields url of order detail page that starts with target date '''
-        start_path = Yes24.order_path
-        # order list page iterator
-        order_list_page_texts = OrderListPage.retrieve_order_list_pages(opener, start_path)
-    
-        for text in order_list_page_texts:
-            orders = OrderListPage.Parse.order_info(text, target_month)
-            if orders == []:
-                break
+    if 'location.replace' not in html:
+        logging.warning('"location.replace" not found in response body. maybe login fail?')
+        return None
+    else:
+        return opener
 
-            for order in orders:
-                yield order
+def verify_login(opener, url):
+    html = opener.open(url).read()
+    # 로그아웃 돼 있으니 login하라고 뜬다
+    if   'Login'  in html:  return False
+    # 로그인 돼 있으니 logout할 수 있다고 뜬다
+    elif 'Logout' in html:  return True
+    else:
+        raise Exception("로그인 여부를 판단할 수 없습니다.")
 
 
-    class Parse:
-        ''' a namespace holding methods to help parsing '''
-        @staticmethod
-        def crop(text):
-            start_pattern = '''<div id="ordList"'''
-            end_pattern   = '''<script language='JavaScript'>'''
+##
+## Generators
+##
+def retrieve_order_list_pages(opener, path):
+    ''' yields text of next order list page '''
+    while path is not None:
+        text = open_url(opener, secure_url + path)
+        yield text
+        # find out next page
+        page_no, path = OrderListPage.navi_info(text)
 
-            start_idx = text.find(start_pattern)
-            if start_idx == -1:
-                raise Exception('cannot find start pattern from: %s' % start_pattern)
+def retrieve_orders(opener, target_month):
+    ''' yields url of order detail page that starts with target date '''
+    start_path = order_path
+    # order list page iterator
+    order_list_page_texts = retrieve_order_list_pages(opener, start_path)
 
-            end_idx = text[start_idx:].find(end_pattern)
-            if end_idx == -1:
-                raise Exception('cannot find end pattern from: %s' % end_pattern)
-            end_idx += start_idx
+    for text in order_list_page_texts:
+        orders = OrderListPage.order_info(text, target_month)
+        if orders == []:
+            break
 
-            text = text[start_idx:end_idx]
-            return text
-
-        @staticmethod
-        def massage(text):
-            text = text.replace(
-                '''<a style="cursor:hand";''', 
-                '''<a style="cursor:hand"'''
-            )
-            return text
-
-        @staticmethod
-        def navi_info(text):
-            # parse
-            text = OrderListPage.Parse.crop(text)
-            text = OrderListPage.Parse.massage(text)
-            soup = BeautifulSoup(text)
-
-            page_navigator_table = soup.table(id="tblNavigator")[0]
-            current_page_anchor  = page_navigator_table.find('a', href=None)
-            next_page_anchor     = current_page_anchor.findNextSibling('a')
-            next_page_href = next_page_anchor["href"] if next_page_anchor else None
-        
-            navi_info = (current_page_anchor.string, next_page_href)
-            logging.debug("current page: #%s, next page: %s" % navi_info)
-            return navi_info
-            
-        @staticmethod
-        def order_info(text, target_month):
-            ''' parse order list page and return list of Order objects '''
-
-            # parse
-            text = OrderListPage.Parse.crop(text)
-            text = OrderListPage.Parse.massage(text)
-            soup = BeautifulSoup(text)
-        
-            # order list
-            orders = []
-            order_list_table  = soup.table(id="MyOrderListTbl")[0]
-            remove_bogus_rows = lambda tag: tag.name == u'tr' and len(tag.findAll('td')) != 1
-            for tr in order_list_table.find('tr').findNextSiblings(remove_bogus_rows):
-                order = Order.build_from_order_list_page(tr, target_month)
-                if order is None:
-                    continue
-                orders.append( order )
-
-            logging.debug("%d orders total." % len(orders))
-            return orders
+        for order in orders:
+            yield order
 
 
 class Order:
     def __init__(self):
-        self.id = None
-        self.price = None
-        self.count = None
-        self.title = None
-        self.date = None
+        self.id      = None
+        self.price   = None
+        self.count   = None
+        self.title   = None
+        self.date    = None
 
         self.payment = None
 
     def page_url(self):
-        return Yes24.get_order_detail_link(self.id)
+        return get_order_detail_link(self.id)
 
     def deliver_state_url(self):
-        return Yes24.get_deliver_state_link(self.id)
+        return get_deliver_state_link(self.id)
 
     @classmethod
     def build_from_order_list_page(cls, tr, target_month):
@@ -221,92 +152,12 @@ class Order:
     
         return order
 
-
     def parse_detail_page(self, opener):
-        text = Yes24.open_url(opener, self.page_url())
-        self.payment = Order.PageParse.parse(text)
+        text = open_url(opener, self.page_url())
+        self.payment = OrderPage.parse(text)
         if self.price != self.payment.price:
             logging.warn("price is different: %s vs %s" % (self.price, self.payment.price))
         return self
-
-
-    class PageParse:
-        ''' namespace holding methods to help parsing '''
-        @staticmethod
-        def massage(text):
-            # lacks double-quote(")
-            text = text.replace(
-                '''<table cellpadding="0" cellspacing=0" border="0" >''', 
-                '''<table cellpadding="0" cellspacing="0" border="0" >'''
-            )
-            return text
-
-        @staticmethod
-        def crop(text):
-            start_pattern = '''<span id="infoQuickDlv"'''
-            end_pattern   = '''<script Language=javascript>'''
-
-            start_idx = text.find(start_pattern)
-            if start_idx == -1:
-                raise Exception('cannot find start pattern from: %s' % start_pattern)
-
-            end_idx = text[start_idx:].find(end_pattern)
-            if end_idx == -1:
-                raise Exception('cannot find end pattern from: %s' % end_pattern)
-            end_idx += start_idx
-
-            text = text[start_idx:end_idx]
-            return text
-
-        @staticmethod
-        def parse(text):
-            payment = Payment()
-
-            text = Order.PageParse.crop(text)
-            text = Order.PageParse.massage(text)
-            soup = BeautifulSoup(text)
-
-            str2int = lambda x: int(x.replace(",", ""))
-        
-            # order price
-            payment.price = soup.find(id="CLbTotOrdAmt").b.string
-            payment.price = str2int(payment.price)
-
-            text = ''.join(filter(lambda x: '<span id="CLbPayPrInfo">' in x, text.split("\r\n"))).strip()
-            text = '<table>%s</table>' % text[text[1:].find('<')+1:-7]
-            soup = BeautifulSoup(text)
-
-            # points saved
-            payment.point_saved = soup.find(attrs={'class':"price"}).b.string
-            payment.point_saved = str2int(payment.point_saved)
-
-            # money spent
-            payment.amount = 0
-            priceB_el = soup.find(attrs={'class':"priceB"})
-            if priceB_el is not None:
-                payment.amount = priceB_el.string
-                payment.amount = str2int(payment.amount)
-
-            # payment method
-            payment.method = None
-            payment_el = soup.find(text=re.compile(u'결제.*수단'))
-            if payment_el is not None:
-                payment.method = payment_el.parent.findNextSibling('td').next.replace("&nbsp;", '').strip()
-
-            # discounts: (name, amount)
-            find_discount = lambda tag: tag.name == u'td' and \
-                tag.findNextSibling('td') and \
-                tag.findNextSibling('td').findNextSibling('td') and \
-                tag.findNextSibling('td').findNextSibling('td').b and \
-                tag.findNextSibling('td').findNextSibling('td').b.string != u'0'
-            payment.discounts = soup.table.table.table.findAll(find_discount)
-            payment.discounts = map(
-                lambda td: (
-                    td.contents[-1].strip(), 
-                    str2int(td.findNextSibling('td').findNextSibling('td').b.string.strip())
-                ), payment.discounts)
-        
-            return payment
 
 
 class Payment:
@@ -317,4 +168,152 @@ class Payment:
         self.amount = None
         self.points_saved = None
 
+#
+# Page parse
+#
+
+class OrderPage:
+    ''' namespace holding methods to help parsing '''
+    @staticmethod
+    def massage(text):
+        # lacks double-quote(")
+        text = text.replace(
+            '''<table cellpadding="0" cellspacing=0" border="0" >''', 
+            '''<table cellpadding="0" cellspacing="0" border="0" >'''
+        )
+        return text
+
+    @staticmethod
+    def crop(text):
+        start_pattern = '''<span id="infoQuickDlv"'''
+        end_pattern   = '''<script Language=javascript>'''
+
+        start_idx = text.find(start_pattern)
+        if start_idx == -1:
+            raise Exception('cannot find start pattern from: %s' % start_pattern)
+
+        end_idx = text[start_idx:].find(end_pattern)
+        if end_idx == -1:
+            raise Exception('cannot find end pattern from: %s' % end_pattern)
+        end_idx += start_idx
+
+        text = text[start_idx:end_idx]
+        return text
+
+    @staticmethod
+    def parse(text):
+        payment = Payment()
+
+        text = OrderPage.crop(text)
+        text = OrderPage.massage(text)
+        soup = BeautifulSoup(text)
+
+        str2int = lambda x: int(x.replace(",", ""))
+    
+        # order price
+        payment.price = soup.find(id="CLbTotOrdAmt").b.string
+        payment.price = str2int(payment.price)
+
+        text = ''.join(filter(lambda x: '<span id="CLbPayPrInfo">' in x, text.split("\r\n"))).strip()
+        text = '<table>%s</table>' % text[text[1:].find('<')+1:-7]
+        soup = BeautifulSoup(text)
+
+        # points saved
+        payment.point_saved = soup.find(attrs={'class':"price"}).b.string
+        payment.point_saved = str2int(payment.point_saved)
+
+        # money spent
+        payment.amount = 0
+        priceB_el = soup.find(attrs={'class':"priceB"})
+        if priceB_el is not None:
+            payment.amount = priceB_el.string
+            payment.amount = str2int(payment.amount)
+
+        # payment method
+        payment.method = None
+        payment_el = soup.find(text=re.compile(u'결제.*수단'))
+        if payment_el is not None:
+            payment.method = payment_el.parent.findNextSibling('td').next.replace("&nbsp;", '').strip()
+
+        # discounts: (name, amount)
+        find_discount = lambda tag: tag.name == u'td' and \
+            tag.findNextSibling('td') and \
+            tag.findNextSibling('td').findNextSibling('td') and \
+            tag.findNextSibling('td').findNextSibling('td').b and \
+            tag.findNextSibling('td').findNextSibling('td').b.string != u'0'
+        payment.discounts = soup.table.table.table.findAll(find_discount)
+        payment.discounts = map(
+            lambda td: (
+                td.contents[-1].strip(), 
+                str2int(td.findNextSibling('td').findNextSibling('td').b.string.strip())
+            ), payment.discounts)
+    
+        return payment
+
+
+
+class OrderListPage:
+    ''' a namespace holding methods to help parsing '''
+    @staticmethod
+    def crop(text):
+        start_pattern = '''<div id="ordList"'''
+        end_pattern   = '''<script language='JavaScript'>'''
+
+        start_idx = text.find(start_pattern)
+        if start_idx == -1:
+            raise Exception('cannot find start pattern from: %s' % start_pattern)
+
+        end_idx = text[start_idx:].find(end_pattern)
+        if end_idx == -1:
+            raise Exception('cannot find end pattern from: %s' % end_pattern)
+        end_idx += start_idx
+
+        text = text[start_idx:end_idx]
+        return text
+
+    @staticmethod
+    def massage(text):
+        text = text.replace(
+            '''<a style="cursor:hand";''', 
+            '''<a style="cursor:hand"'''
+        )
+        return text
+
+    @staticmethod
+    def navi_info(text):
+        # parse
+        text = OrderListPage.crop(text)
+        text = OrderListPage.massage(text)
+        soup = BeautifulSoup(text)
+
+        page_navigator_table = soup.table(id="tblNavigator")[0]
+        current_page_anchor  = page_navigator_table.find('a', href=None)
+        next_page_anchor     = current_page_anchor.findNextSibling('a')
+        next_page_href = next_page_anchor["href"] if next_page_anchor else None
+    
+        navi_info = (current_page_anchor.string, next_page_href)
+        logging.debug("current page: #%s, next page: %s" % navi_info)
+        return navi_info
+        
+    @staticmethod
+    def order_info(text, target_month):
+        ''' parse order list page and return list of Order objects '''
+
+        # parse
+        text = OrderListPage.crop(text)
+        text = OrderListPage.massage(text)
+        soup = BeautifulSoup(text)
+    
+        # order list
+        orders = []
+        order_list_table  = soup.table(id="MyOrderListTbl")[0]
+        remove_bogus_rows = lambda tag: tag.name == u'tr' and len(tag.findAll('td')) != 1
+        for tr in order_list_table.find('tr').findNextSiblings(remove_bogus_rows):
+            order = Order.build_from_order_list_page(tr, target_month)
+            if order is None:
+                continue
+            orders.append( order )
+
+        logging.debug("%d orders total." % len(orders))
+        return orders
 
--- yes24_gen.py
+++ yes24_gen.py
@@ -5,7 +5,8 @@ import logging
 import sys, datetime
 import locale; locale.setlocale(locale.LC_ALL, '')
 
-from yes24 import Yes24, OrderListPage
+import yes24
+from yes24 import OrderListPage
 
 def num(value, comma=True):
     ''' format number with comma '''
@@ -30,7 +31,7 @@ def group_by(dic_list):
 def authorize():
     username = raw_input('Username: ')
     password = getpass.getpass()
-    opener = Yes24.authorize(username, password)
+    opener = yes24.authorize(username, password)
     del username, password
     if opener is None:
         logging.error("failed to login!")
@@ -38,15 +39,19 @@ def authorize():
     return opener
 
 def report_order_summary(orders):
+    orders = list(orders)
     earliest_date = min(order.date  for order in orders)
     latest_date   = max(order.date  for order in orders)
     price_sum     = sum(order.price for order in orders)
     pkg_count     = sum(order.count for order in orders)
     print u"%s ~ %s 동안 %d번 주문: 총 %s개, %s원" % \
         (earliest_date, latest_date, len(orders), pkg_count, num(price_sum))
+    return orders
 
 def report_order_detail(orders):
-    for order in sorted(orders, key=lambda order: order.date):
+    _orders = []
+    #orders = sorted(orders, key=lambda order: order.date)
+    for order in orders:
         payment = order.payment
         print ' *',
         print u"%s:" % order.date,
@@ -65,27 +70,31 @@ def report_order_detail(orders):
                 print u"+ %s %6s원"  % (discount_by, num(discount_amt)),
             print u")",
         print u"/ %s점 적립" % num(payment.point_saved)
+        _orders.append(order)
     print
+    return _orders
 
 def report_payment_summary(orders):
+    orders = list(orders)
     payments = [order.payment for order in orders]
     point_saved     = sum(payment.point_saved for payment in payments)
     payment_groups  = group_by({p.method: p.amount} for p in payments if p.method)
     discount_groups = group_by(dict(p.discounts) for p in payments)
     
     # report
-    print u"정산"
     for paid in [payment_groups, discount_groups, {u"적립": [point_saved]}]:
         for method, amounts in paid.iteritems():
             print u'* %s: %7s원' % (method, num(sum(amounts)))
     print
 
-    # check 
+    # check price integrity
     price_sum    = sum(order.price for order in orders)
     payment_sum  = sum(p.amount for p in payments)
     discount_sum = sum(d[1] for p in payments for d in p.discounts)
     if price_sum != payment_sum + discount_sum:
         logging.warn("paid sum %d does not match price sum %d!" % (num(payment_sum + discount_sum), num(price_sum)))
+
+    return orders
 
 
 if __name__ == '__main__':
@@ -95,16 +104,15 @@ if __name__ == '__main__':
         logging.basicConfig(level=logging.DEBUG)
 
     opener = authorize()
-    # orders from order list page
-    orders = OrderListPage.retrieve_orders(opener, target_month=target_month)
-    # order detail page
+    # retrieve orders from order list page
+    orders = yes24.retrieve_orders(opener, target_month=target_month)
+    # parse order from order detail page
     orders = (order.parse_detail_page(opener) for order in orders)
     
     # report
-    orders = list(orders) # should materilize iterator, for reuse
-    report_order_summary(orders)
-    report_order_detail(orders)
-    report_payment_summary(orders)
+    orders = report_order_detail(orders)
+    orders = report_order_summary(orders)
+    orders = report_payment_summary(orders)
 
-    print 'opened', Yes24.open_url_count, 'times, reading', num(Yes24.read_bytes), 'bytes'
+    print 'opened', yes24.open_url_count, 'times, reading', num(yes24.read_bytes), 'bytes'
 
